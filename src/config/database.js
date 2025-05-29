e58159a76bfdf98a27d2b041c21a67b9e34fc674
LuizPauloJRM <luizpaulomedeiros77@hotmail.com>
2025-05-28 21:05:52 -0300

    migration primeira

--- src/config/database.js
+++ src/config/database.js
@@ -1,13 +1,13 @@
-module.exports ={
-    dialect: 'postgres',
-    host: 'localhost',
-    port: 5433,
-    username: 'postgres',   
-    password: 'postgres',
-    database: 'devburger',
-    define:{
-        timestamps: true, // Adiciona createdAt e updatedAt
-        underscored: true, // Utiliza snake_case para os nomes das colunas
-        underscoredAll: true, // Utiliza snake_case para os nomes das tabelas
-    }
+module.exports = {
+  dialect: 'postgres',
+  host: 'localhost',
+  port: 5433,
+  username: 'postgres',
+  password: 'postgres',
+  database: 'devburger',
+  define: {
+    timestamps: true,   // Automatically add createdAt and updatedAt fields
+    underscored: true,  // Use snake_case for column names
+    underscoredAll: true,   // Use snake_case for all table names
+  }
 };
