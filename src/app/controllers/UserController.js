2b3880bcf4af106201df1888218cfc269e5a447c
LuizPauloJRM <luizpaulomedeiros77@hotmail.com>
2025-05-30 01:23:56 -0300

    try catch do id name email password admin

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -7,28 +7,49 @@
  */
 import { v4 } from 'uuid';
 import User from '../models/User';
+import * as Yup from 'yup';
 
 class UserController {
     async store(request, response) {
+        const schema = Yup.object({
+            name: Yup.string().required(),
+            email: Yup.string().email().required(),
+            password_hash: Yup.string().min(6).required(),
+            admin: Yup.boolean(),
+        });
+        try {
+            schema.validateSync(request.body, { abortEarly: false });
+        } catch (err) {
+            return response.status(400).json({ error: err.errors });
+        }
+
+
+        /*const validation = await schema.isValid(request.body);
+        console.log(validation);*/
+
+        /*if (!isValid) {
+            return response.status(400).json({ error: 'Make sure data is correct' })
+        }*/
         const { name, email, password, admin } = request.body;
 
-        try {
-            const user = await User.create({
-                id: v4(),
-                name,
-                email,
-                password,
-                admin,
-            });
-            return response.status(201).json({
-                id: user.id,
-                name: user.name,
-                email: user.email,
-                admin: user.admin,
-            });
-        } catch (error) {
+
+        //try{
+        const user = await User.create({
+            id: v4(),
+            name,
+            email,
+            password,
+            admin,
+        });
+        return response.status(201).json({
+            id: user.id,
+            name: user.name,
+            email: user.email,
+            admin: user.admin,
+        });
+        /*} catch (error) {
             return response.status(500).json({ error: "Internal server error", details: error.message });
-        }
+        }*/
     }
 }
 export default new UserController();
