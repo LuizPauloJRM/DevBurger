fa31339a3bc70190dcf297a49cedd7704d7a2a3f
LuizPauloJRM <luizpaulomedeiros77@hotmail.com>
2025-05-30 13:37:05 -0300

    criei Controller de produtos

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,13 @@
 import { Router } from 'express';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import ProductController from './app/controllers/ProductController';
 //import User from './app/models/User';
 
 const routes = new Router();
 
 routes.post('/users', UserController.store);             // Criar usuário
 routes.post('/session', SessionController.store); // Login de usuário 
+routes.post('/products', ProductController.store);// Products
 
 export default routes;
